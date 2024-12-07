4f8e99ca1b90b942b85028d2c8f9cb7da44415db
Satvik <katochsatvik@gmail.com>
2024-12-07 10:44:30 +0530

    LogoTicker section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/sections/Hero";
 import { Header } from "@/sections/Header";
+import { LogoTicker } from "@/sections/LogoTicker";
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <LogoTicker />
     </>
   );
 }
