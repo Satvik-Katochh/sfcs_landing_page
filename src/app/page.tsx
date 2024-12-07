ad17cec6a50cf8fb574deb72ad95ee6101170875
Satvik <katochsatvik@gmail.com>
2024-12-07 11:19:48 +0530

    Product showcase done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Hero } from "@/sections/Hero";
 import { Header } from "@/sections/Header";
 import { LogoTicker } from "@/sections/LogoTicker";
+import { ProductShowcase } from "@/sections/ProductShowcase";
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
       <LogoTicker />
+      <ProductShowcase />
     </>
   );
 }
