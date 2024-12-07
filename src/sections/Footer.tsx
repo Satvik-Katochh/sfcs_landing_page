832b8da312fa14a988a2dcb843dd3a7f87fbf06d
Satvik <katochsatvik@gmail.com>
2024-12-07 15:34:12 +0530

    CallToAction Section Done

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+import logo from "@/assets/logosaas.png";
+import SocialX from "@/assets/social-x.svg";
+import SocialInsta from "@/assets/social-insta.svg";
+import SocialLinkedIn from "@/assets/social-linkedin.svg";
+import SocialPin from "@/assets/social-pin.svg";
+import SocialYoutube from "@/assets/social-youtube.svg";
 export const Footer = () => {
-  return null;
+  return (
+    <footer>
+      <div className="container">
+        <Image src={logo} height={40} alt="Saas logo" />
+        <nav>
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Carrers</a>
+        </nav>
+      </div>
+    </footer>
+  );
 };
